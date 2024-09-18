8bb7d0c4bc4a6c552795db9ca55a07c422cffe9d
Darren Kirby <bulliver@gmail.com>
2024-09-18 15:31:30 -0700

    Updated to use revamped getfsstat_ext()

--- lib/test.c
+++ lib/test.c
@@ -2,22 +2,25 @@
 
 int main(void) {
     int n_mounts = 0;
-    struct statfs_ext *buf = malloc(sizeof(struct statfs_ext));
-    if (buf == NULL) {
-        perror("malloc failed");
-        exit(EXIT_FAILURE);
-    }
-    
-    n_mounts = getfsstat_ext(buf, FS_ALL, 0);
+    struct statfs_ext *buf = NULL;
+
+    /* First call: Get the number of mounted filesystems */
+    n_mounts = getfsstat_ext(&buf, FS_ALL, 0);
     if (n_mounts == -1) {
         printf("ERRNO: %i\n", errno);
         exit(EXIT_FAILURE);
     }
 
-    printf("Found %i mounted file systems:\n\n", n_mounts);
+    printf("Found %i mounted file systems.\n\n", n_mounts);
 
-    int i;
-    for (i = 0; i < n_mounts; i++) {
+    /* Second call: Allocate buffer and get the filesystem data */
+    n_mounts = getfsstat_ext(&buf, n_mounts * sizeof(struct statfs_ext), 0);
+    if (n_mounts == -1) {
+        printf("ERRNO: %i\n", errno);
+        exit(EXIT_FAILURE);
+    }
+
+    for (int i = 0; i < n_mounts; i++) {
         printf("Mounted Filesystem: %s\n", buf[i].f_mntfromname);
         printf("\tmnt point: %s\n", buf[i].f_mntonname);
         printf("\tfs type name: %s\n", buf[i].f_fstypename);
@@ -28,9 +31,8 @@ int main(void) {
         printf("\tfree blocks user: %d\n", (int)buf[i].f_bavail);
         printf("\ttotal nodes: %d\n", (int)buf[i].f_files);
         printf("\tfree nodes: %d\n\n", (int)buf[i].f_ffree);
-
     }
 
+    free(buf);  // Free the allocated memory
     return 0;
 }
-
